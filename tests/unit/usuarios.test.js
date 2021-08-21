255a3d7452e8c6abfc4e30123b0fea9c858eacf2
Samanta Fluture <samantafluture@gmail.com>
2021-08-21 15:19:41 -0300

    create unit tests for usuarios model

--- tests/unit/usuarios.test.js
+++ tests/unit/usuarios.test.js
@@ -1,36 +1,34 @@
 const bcrypt = require('bcrypt');
-const models = require('../../api/models');
 const { Usuarios } = require('../../api/models');
 
 describe('Usuários', () => {
-    // it('deve criar uma senha hash válida para o usuário', async () => {
-    //     const usuario = await Usuarios.create({
-    //         nome: 'Teste 1',
-    //         email: 'teste@email.com',
-    //         senha: '12345678'
-    //     });
-    //     const compareHash = await bcrypt.compare('12345678', usuario.senha);
-    //     expect(compareHash).toBe(true);
-    // });
-    // it('deve verificar se o usuário existe via email', async () => {
-    //     const usuario = await models.Usuarios.findOne({
-    //         where: { email: 'teste@email.com' }
-    //     });
-    //     const emailUsuario = usuario.email;
-    //     expect(emailUsuario).toMatch('teste@email.com');
-    // });
+    it('deve criar uma senha hash válida para o usuário', async () => {
+        const usuario = await Usuarios.create({
+            nome: 'Rosa Fluture',
+            email: 'rosa@email.com',
+            senha: '12345678'
+        });
+        const compareHash = await bcrypt.compare('12345678', usuario.senha);
+        expect(compareHash).toBe(true);
+    });
+    it('deve verificar se o usuário existe via email', async () => {
+        const usuario = await Usuarios.findOne({
+            where: { email: 'ana@email.com' }
+        });
+        expect(usuario.email).toMatch('ana@email.com');
+    });
     it('deve validar que o email preenchido é válido', async () => {
         const usuario = await Usuarios.create({
-            nome: 'Antônia Fluture',
-            email: 'antonia@email.com',
+            nome: 'Nancy Fluture',
+            email: 'nancy@email.com',
             senha: '12345678'
         });
         expect(usuario.email).toContain('@');
     });
     it('deve validar que nome foi preenchido e tem pelo menos 3 caracteres', async () => {
         const usuario = await Usuarios.create({
-            nome: 'Ana',
-            email: 'ana@email.com',
+            nome: 'Zoe',
+            email: 'zozo@email.com',
             senha: '12345678'
         });
         expect(usuario.nome).toHaveLength(3);
