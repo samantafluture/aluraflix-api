0ba7374e15726557b6a57eb31d6c6b75b6fa0fd8
Samanta Fluture <samantafluture@gmail.com>
2021-08-23 12:04:46 -0300

    add use_env_variable for db heroku

--- api/config/config.js
+++ api/config/config.js
@@ -3,11 +3,13 @@ require('dotenv').config({
 });
 
 module.exports = {
-    use_env_variable: process.env.DATABASE_URL,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
-    storage: './tests/database.sqlite'
+    // eslint-disable-next-line no-undef
+    use_env_variable: DATABASE_URL
+    // use_env_variable: process.env.DATABASE_URL,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // dialect: process.env.DB_DIALECT || 'mysql',
+    // storage: './tests/database.sqlite'
 };
--- api/models/index.js
+++ api/models/index.js
@@ -14,24 +14,17 @@ const basename = path.basename(__filename);
 const config = require(__dirname + '/../config/config.js');
 const db = {};
 
-const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-);
-
-// let sequelize;
-// if (config.use_env_variable) {
-//     sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//     sequelize = new Sequelize(
-//         config.database,
-//         config.username,
-//         config.password,
-//         config
-//     );
-// }
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
+    );
+}
 
 fs.readdirSync(__dirname)
     .filter((file) => {
