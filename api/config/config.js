cde1d95580d3064132cfe9dd60fb995908e8a545
Samanta Fluture <samantafluture@gmail.com>
2021-08-23 13:21:43 -0300

    sequelize config for heroku

--- api/config/config.js
+++ api/config/config.js
@@ -1,6 +1,6 @@
-require('dotenv').config({
-    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
-});
+// require('dotenv').config({
+//     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+// });
 
 module.exports = {
     use_env_variable: 'DATABASE_URL'
