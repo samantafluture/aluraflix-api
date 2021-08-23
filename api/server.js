1afe90bfdb98d8216e554ebb30c2df98e150a48e
Samanta Fluture <samantafluture@gmail.com>
2021-08-23 11:19:37 -0300

    alter script start on package.json

--- api/server.js
+++ api/server.js
@@ -1,7 +1,3 @@
-require('dotenv').config();
 const app = require('./app');
 
-const port = 3000;
-
-app.listen(process.env.PORT || port, () =>
-    console.log(`Servidor rodando na porta ${port}`));
+app.listen(process.env.PORT || 3000);
