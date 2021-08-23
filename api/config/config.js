ff451de8b207400aa0e5525b75eb65211f6d878e
Samanta Fluture <samantafluture@gmail.com>
2021-08-23 11:51:56 -0300

    add use_env_variable for db heroku

--- api/config/config.js
+++ api/config/config.js
@@ -3,10 +3,11 @@ require('dotenv').config({
 });
 
 module.exports = {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
-    storage: './tests/database.sqlite'
+    use_env_variable: DATABASE_URL,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // dialect: process.env.DB_DIALECT || 'mysql',
+    // storage: './tests/database.sqlite'
 };
