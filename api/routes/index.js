652e829379b207cfd87b03bdfca8d4a0b58e589d
Samanta Fluture <samantafluture@gmail.com>
2021-08-23 15:53:16 -0300

    update readme.md file

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,7 +9,9 @@ module.exports = (app) => {
     app.use(express.json(), categorias, videos, usuarios, auth);
 
     app.get('/', (req, res) => {
-        res.json('Bem-vindo a Aluraflix API!');
+        res.json(
+            'Bem-vindo a Aluraflix API! Acesse as rotas sem autenticação: /videos/free e /categorias/free.'
+        );
     });
     app.get('/terms', (req, res) => {
         res.json('Termos de serviço');
