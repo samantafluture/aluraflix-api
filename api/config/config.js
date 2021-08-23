f952128cb3e48a36a2924963835a3d5454088d0e
Samanta Fluture <samantafluture@gmail.com>
2021-08-23 11:54:44 -0300

    add use_env_variable for db heroku

--- api/config/config.js
+++ api/config/config.js
@@ -3,7 +3,8 @@ require('dotenv').config({
 });
 
 module.exports = {
-    use_env_variable: DATABASE_URL,
+    // eslint-disable-next-line no-undef
+    use_env_variable: DATABASE_URL
     // username: process.env.DB_USER,
     // password: process.env.DB_PASS,
     // database: process.env.DB_NAME,
